aafaf6a5abf6429f1a870318e5d8206cfd1aec69
iancorcoran <ian@stormpath.com>
2016-08-01 16:47:07 -0700

    IT fix

--- extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/http/httpclient/HttpClientRequestExecutorIT.groovy
+++ extensions/httpclient/src/test/groovy/com/stormpath/sdk/impl/http/httpclient/HttpClientRequestExecutorIT.groovy
@@ -1,7 +1,7 @@
 package com.stormpath.sdk.impl.http.httpclient
 
 import com.stormpath.sdk.account.Account
-import com.stormpath.sdk.client.AuthenticationScheme
+import com.stormpath.sdk.client.AuthenticationSchemes
 import com.stormpath.sdk.client.ClientIT
 import com.stormpath.sdk.http.HttpMethod
 import com.stormpath.sdk.http.Response
@@ -51,7 +51,7 @@ class HttpClientRequestExecutorIT extends ClientIT {
         // this proves that we are *not* waiting on redirects
         def verifyUri = app.getHref() + "/authTokens/" + result.getAccessTokenString()
 
-        def httpClientRequestExecutor = new HttpClientRequestExecutor(client.getApiKey(), null, AuthenticationScheme.SAUTHC1, 2000)
+        def httpClientRequestExecutor = new HttpClientRequestExecutor(client.getClientCredentials(), null, AuthenticationSchemes.getAuthenticationScheme(AuthenticationSchemes.SAUTHC1), 2000)
         httpClientRequestExecutor.setNumRetries(0)
 
         Callable<Response> callable = new Callable<Response>() {
